a7b98112721997e9f7a2d7dc68b2544466ed4b51
dan_kufra <dankufra@gmail.com>
2019-07-31 12:54:44 +0300

    BRCA receptorStatusPrediction project commit

--- receptorStatusPrediction.py
+++ receptorStatusPrediction.py
@@ -1,6 +1,7 @@
 from utils import*
 from sklearn.svm import SVC
 
+print("Reading data")
 DIAG_MAPPING = {'Positive' : 1, 'Negative': -1,
                 'Indeterminate': 0, 'Equivocal': 0, '[Not Available]': -2, 'Not Performed': -2, '[Not Evaluated]' : -2}
 
@@ -42,26 +43,38 @@ final_df['her2_ihc_level'] = final_df['her2_ihc_score'].map(LEVEL_SCORE_MAPPING)
 # How many of each measurement type we have
 
 # Crosstab between her2 ihc status and fish status
-pd.crosstab(final_df['her2_ihc'], final_df['her2_fish'])
+print("her2_ihc vs her2_fish")
+print(pd.crosstab(final_df['her2_ihc'], final_df['her2_fish']))
+
 
 # get list of patients where her2 fish status does not match ihc status
 fish_vs_ihc = final_df[((final_df['her2_ihc'] == 1) & (final_df['her2_fish'] == -1)) | ((final_df['her2_ihc'] == -1) & (final_df['her2_fish'] == 1))]
+print("%d patients with mismatch between her2_ihc and her2_fish" %len(fish_vs_ihc))
 
 # Crosstab between her2 ihc status and ihc score
-pd.crosstab(final_df['her2_ihc'], final_df['her2_ihc_level'])
+print("her2_ihc_status vs her2_ihc_score")
+print(pd.crosstab(final_df['her2_ihc'], final_df['her2_ihc_level']))
 
 # get list of patients where her2 ihc level does not match ihc status
 ihc_status_vs_ihc_level = final_df[((final_df['her2_ihc'] == 1) & (final_df['her2_ihc_level'] == -1)) | ((final_df['her2_ihc'] == -1) & (final_df['her2_ihc_level'] == 1))]
+print("%d patients with mismatch between her2_ihc_status and her2_ihc_score" %len(ihc_status_vs_ihc_level))
+
 
 # Replacing HER2 (IHC) status by HER2 (FISH) (a more accurate test) whenever available:
+print("Replacing her2_ihc by her2_fish where available")
 final_df['her2_ihc_and_fish'] = final_df['her2_ihc']
 final_df.loc[final_df['her2_fish'] != -2,'her2_ihc_and_fish'] = final_df.loc[final_df['her2_fish'] != -2, 'her2_fish']
 
 # Crosstab between her2 ihc status and ihc status with fish
-pd.crosstab(final_df['her2_ihc'], final_df['her2_ihc_and_fish'])
+print("her2_ihc vs her2_ihc_fish")
+print(pd.crosstab(final_df['her2_ihc'], final_df['her2_ihc_and_fish']))
 
+
+er_vs_pr = final_df[((final_df['er_ihc'] == 1) & (final_df['pr_ihc'] == -1)) | ((final_df['er_ihc'] == -1) & (final_df['pr_ihc'] == 1))]
+print("%d patients with mismatch between er_ihc and pr_ihc" %len(er_vs_pr))
 
 # create Triple Negative vs not Triple Negative dataset
+print("Creating Triple Negative vs not Triple negative labels")
 df_clinical = pd.DataFrame(final_df[np.hstack([final_df.columns[['cg' in col for col in final_df.columns]],['er_ihc', 'pr_ihc', 'her2_ihc_and_fish', 'her2_ihc', 'her2_fish', 'her2_ihc_level']])])
 
 df_clinical['neg'] = (df_clinical['er_ihc'] == -1) & (df_clinical['pr_ihc'] == -1) & (df_clinical['her2_ihc_and_fish'] == -1)
@@ -74,7 +87,9 @@ df_clinical['NA_pre_fish'] = (df_clinical['er_ihc'] == -2) | (df_clinical['pr_ih
 
 # drop patients who are NA and not pos
 # df_clinical.drop(df_clinical[df_clinical['NA'] & np.logical_not(df_clinical['pos'])].index, axis=0, inplace=True)
+print("Removing patients with NA")
 df_clinical.drop(df_clinical[df_clinical['NA'] == True].index, axis=0, inplace=True)
+
 # Create labels
 Y = np.zeros(df_clinical.shape[0])
 Y[df_clinical.pos] = 1
@@ -90,6 +105,7 @@ Keep the following in test:
 2. Observations where IHC level and IHC status did not match
 """
 
+print("Splitting data in to train/test")
 train_idx =  (df_clinical['neg_pre_fish'] != df_clinical['neg']) |  \
              (df_clinical['pos_pre_fish'] != df_clinical['pos'])
 train_idx =  train_idx | ((df_clinical['her2_ihc'] != df_clinical['her2_ihc_level']) &
@@ -106,111 +122,147 @@ Y_test = Y[shuf_test_idx]
 Y_train = Y[shuf_train_idx]
 X_test = X[shuf_test_idx]
 X_train = X[shuf_train_idx]
+
+print("Running SVM on data")
 clf = SVC(class_weight='balanced', kernel='linear')
 clf.fit(X_train, Y_train)
 
 pred_test = clf.predict(X_test)
 pred_train = clf.predict(X_train)
 
-rel_cols = ['er_ihc', 'pr_ihc', 'her2_ihc', 'her2_fish', 'her2_ihc_level', 'pos', 'her2_ihc_and_fish']
-patients_changed_by_fish = df_clinical.iloc[np.where((df_clinical['neg_pre_fish'] != df_clinical['neg']) |
-                                                     (df_clinical['pos_pre_fish'] != df_clinical['pos']))][rel_cols]
+print("test ACC: %f" %(np.sum(pred_test == Y_test).astype(np.float32)/ Y_test.shape[0]))
 
-patients_with_ihc_level_diff = df_clinical.iloc[np.where(((df_clinical['her2_ihc'] != df_clinical['her2_ihc_level']) &
-                                                          (df_clinical['her2_ihc_level'] != -2) &
-                                                          ((df_clinical['her2_fish'] == -2) | (df_clinical['her2_fish'] == 0))))][rel_cols]
-
-patients_wrong_test = df_clinical.iloc[shuf_test_idx[np.where(pred_test != Y_test)]][rel_cols]
-patients_wrong_train = df_clinical.iloc[shuf_train_idx[np.where(pred_train != Y_train)]][rel_cols]
-
+print("Running random forest:")
 clf = RandomForestClassifier(random_state=666, max_depth=3, n_estimators=10)
 clf = clf.fit(X_train, Y_train)
 pred_rf_test = clf.predict(X_test)
 pred_rf_train = clf.predict(X_train)
 
+print("test ACC: %f" %(np.sum(pred_rf_test == Y_test).astype(np.float32)/ Y_test.shape[0]))
+
+
+rel_cols = ['er_ihc', 'pr_ihc', 'her2_ihc', 'her2_fish', 'her2_ihc_level', 'pos', 'her2_ihc_and_fish']
+patients_changed_by_fish = df_clinical.iloc[np.where((df_clinical['neg_pre_fish'] != df_clinical['neg']) |
+                                                     (df_clinical['pos_pre_fish'] != df_clinical['pos']))][rel_cols]
+print("Patients whose label changed by fish update:")
+print(patients_changed_by_fish)
+
+
+print("Patients whose er_ihc mismatches pr_ihc and changes label:")
+patients_changed_by_mismatch = df_clinical.iloc[np.where((df_clinical['pos'] == 1) & (((df_clinical['er_ihc'] == 1) & (df_clinical['pr_ihc'] == -1)) | ((df_clinical['er_ihc'] == -1) & (df_clinical['pr_ihc'] == 1))) & (df_clinical['her2_ihc_and_fish'] == -1))][rel_cols]
+
+patients_with_ihc_level_diff = df_clinical.iloc[np.where(((df_clinical['her2_ihc'] != df_clinical['her2_ihc_level']) &
+                                                          (df_clinical['her2_ihc_level'] != -2) &
+                                                          ((df_clinical['her2_fish'] == -2) | (df_clinical['her2_fish'] == 0))))][rel_cols]
+
+print("Patients whose IHC level mismatches status (and no fish used):")
+print(patients_with_ihc_level_diff)
+
+
+patients_wrong_test = df_clinical.iloc[shuf_test_idx[np.where(pred_test != Y_test)]][rel_cols]
+patients_wrong_train = df_clinical.iloc[shuf_train_idx[np.where(pred_train != Y_train)]][rel_cols]
+
 patients_wrong_test_rf = df_clinical.iloc[shuf_test_idx[np.where(pred_rf_test != Y_test)]][rel_cols]
 patients_wrong_train_rf = df_clinical.iloc[shuf_train_idx[np.where(pred_rf_train != Y_train)]][rel_cols]
 
-pred_rf_train.index.name = 'patient_name'
-pred_rf_test.index.name = 'patient_name'
+
+# pred_rf_train.index.name = 'patient_name'
+# pred_rf_test.index.name = 'patient_name'
 patients_wrong_test_rf.index.name = 'patient_name'
 patients_wrong_train_rf.index.name = 'patient_name'
 patients_changed_by_fish.index.name = 'patient_name'
-patients_wrong_train_rf.join(patients_changed_by_fish, lsuffix='new', how='inner')
+
+print("Patients changed by fish that we misclassified")
+print(patients_wrong_train_rf.join(patients_changed_by_fish, lsuffix='_new', how='inner'))
+print(patients_wrong_test_rf.join(patients_changed_by_fish, lsuffix='_new', how='inner'))
+
+
+# Attempt to classify er, pr, her2 seperately:
+er_pr_mismatch = df_clinical.iloc[np.where(((df_clinical['er_ihc'] == 1) & (df_clinical['pr_ihc'] == -1)) | ((df_clinical['er_ihc'] == -1) & (df_clinical['pr_ihc'] == 1)))][rel_cols]
+
+train_idx = np.zeros(df_clinical.shape[0], dtype=np.bool)
+train_idx[np.random.choice(np.arange(df_clinical.shape[0]), int(df_clinical.shape[0] * 0.8))] = True
+shuf_test_idx = np.random.permutation(np.where(~train_idx)[0])
+shuf_train_idx = np.random.permutation(np.where(train_idx)[0])
+
+X_test = X[shuf_test_idx]
+X_train = X[shuf_train_idx]
+
+# ER
+Y_er = np.zeros(df_clinical.shape[0])
+Y_er[df_clinical.er_ihc == 1] = 1
+
+Y_test_er = Y_er[shuf_test_idx]
+Y_train_er = Y_er[shuf_train_idx]
+
+print("Running SVM on data")
+clf_er = SVC(class_weight='balanced', kernel='linear')
+clf_er.fit(X_train, Y_train_er)
+
+pred_test_er = clf_er.predict(X_test)
+pred_train_er = clf_er.predict(X_train)
+
+print("test ACC er_ihc: %f" %(np.sum(pred_test_er == Y_test_er).astype(np.float32)/ Y_test_er.shape[0]))
+
+patients_wrong_test_er = df_clinical.iloc[shuf_test_idx[np.where(pred_test_er != Y_test_er)]][rel_cols]
+patients_wrong_train_er = df_clinical.iloc[shuf_train_idx[np.where(pred_train_er != Y_train_er)]][rel_cols]
+
+patients_wrong_test_er.index.name = 'patient_name'
+patients_wrong_train_er.index.name = 'patient_name'
+er_pr_mismatch.index.name = 'patient_name'
+
+print("er wrong pred in mismatch with pr")
+print(patients_wrong_train_er.join(er_pr_mismatch, lsuffix='_new', how='inner'))
+print(patients_wrong_test_er.join(er_pr_mismatch, lsuffix='_new', how='inner'))
+
+
+# PR
+Y_pr = np.zeros(df_clinical.shape[0])
+Y_pr[df_clinical.pr_ihc == 1] = 1
+
+Y_test_pr = Y_pr[shuf_test_idx]
+Y_train_pr = Y_pr[shuf_train_idx]
+
+print("Running SVM on data")
+clf_pr = SVC(class_weight='balanced', kernel='linear')
+clf_pr.fit(X_train, Y_train_pr)
+
+pred_test_pr = clf_pr.predict(X_test)
+pred_train_pr = clf_pr.predict(X_train)
+
+print("test ACC er_ihc: %f" %(np.sum(pred_test_pr == Y_test_pr).astype(np.float32)/ Y_test_pr.shape[0]))
+
+patients_wrong_test_pr = df_clinical.iloc[shuf_test_idx[np.where(pred_test_pr != Y_test_pr)]][rel_cols]
+patients_wrong_train_pr = df_clinical.iloc[shuf_train_idx[np.where(pred_train_pr != Y_train_pr)]][rel_cols]
+
+patients_wrong_test_pr.index.name = 'patient_name'
+patients_wrong_train_pr.index.name = 'patient_name'
+er_pr_mismatch.index.name = 'patient_name'
+
+print("er wrong pred in mismatch with pr")
+print(patients_wrong_train_pr.join(er_pr_mismatch, lsuffix='_new', how='inner'))
+print(patients_wrong_test_pr.join(er_pr_mismatch, lsuffix='_new', how='inner'))
+
+
+# her2
+Y_her2 = np.zeros(df_clinical.shape[0])
+Y_her2[df_clinical.her2_ihc_and_fish == 1] = 1
+
+Y_test_her2 = Y_her2[shuf_test_idx]
+Y_train_her2 = Y_her2[shuf_train_idx]
+
+print("Running SVM on data")
+clf_her2 = SVC(class_weight='balanced', kernel='linear')
+clf_her2.fit(X_train, Y_train_her2)
+
+pred_test_her2 = clf_her2.predict(X_test)
+pred_train_her2 = clf_her2.predict(X_train)
+
+print("test ACC er_ihc: %f" %(np.sum(pred_test_her2 == Y_test_her2).astype(np.float32)/ Y_test_her2.shape[0]))
 
 import pdb
 pdb.set_trace()
 
-
-
-
-#['er_ihc', 'pr_ihc', 'her2_ihc', 'her2_fish', 'her2_ihc_level']
-
-# weird_combinations = [[ 1,   -1,   1],
-#                       [-1,    1,   1],
-#                       [ 1,   -1,  -1],
-#                       [-1,    1,  -1],
-#                       [ 1,    0,   1],
-#                       [ 1,    0,  -1],
-#                       [ 0,    1,   1],
-#                       [ 0,    1,  -1],
-#                       [ 1,    1,   0],
-#                       [-1,   -1,   0],
-#                       [ 1,   -1,   0],
-#                       [-1,    1,   0],
-#                       [ 0,    0,   0],
-#                       [ 0,    0,   1],
-#                       [ 0,    0,  -1]]
-#
-# # find weird stuff (i.e er differs from pr and her2_ihc differs from her2_fish)
-# for comb in weird_combinations:
-#     num_samples = final_df[(final_df['er_status_by_ihc'] == comb[0]) & (final_df['pr_status_by_ihc'] == comb[1]) & (final_df['her2_status_by_ihc'] == comb[2])].shape[0]
-#     print("%f samples in weird combination: er_status=%f,pr_status=%f,her2_status=%f"% (num_samples, comb[0], comb[1], comb[2]))
-# print("##############################################")
-#
-# combinations = [[ 1,   1,   1],
-#                 [ 1,   1,  -1],
-#                 [ 1,  -1,   1],
-#                 [ 1,  -1,  -1],
-#                 [-1,   1,   1],
-#                 [-1,   1,  -1],
-#                 [-1,  -1,   1],
-#                 [-1,  -1,  -1]]
-# # get some statistics on different amounts of cancer types we have
-# for comb in combinations:
-#     num_samples = final_df[(final_df['er_status_by_ihc'] == comb[0]) &
-#                            (final_df['pr_status_by_ihc'] == comb[1]) &
-#                            ((final_df['her2_status_by_ihc'] == comb[2]) | ((final_df['her2_status_by_ihc'] == 0) & (final_df['her2_fish_status'] == comb[2])))].shape[0]
-#     print("%f samples in combination: er_status=%f,pr_status=%f,her2_status=%f"% (num_samples, comb[0], comb[1], comb[2]))
-
-# 9.000000 samples in weird combination: er_status=1.000000,pr_status=-1.000000,her2_status=1.000000
-# 3.000000 samples in weird combination: er_status=-1.000000,pr_status=1.000000,her2_status=1.000000
-# 50.000000 samples in weird combination: er_status=1.000000,pr_status=-1.000000,her2_status=-1.000000
-# 8.000000 samples in weird combination: er_status=-1.000000,pr_status=1.000000,her2_status=-1.000000
-# 0.000000 samples in weird combination: er_status=1.000000,pr_status=0.000000,her2_status=1.000000
-# 1.000000 samples in weird combination: er_status=1.000000,pr_status=0.000000,her2_status=-1.000000
-# 0.000000 samples in weird combination: er_status=0.000000,pr_status=1.000000,her2_status=1.000000
-# 0.000000 samples in weird combination: er_status=0.000000,pr_status=1.000000,her2_status=-1.000000
-# 160.000000 samples in weird combination: er_status=1.000000,pr_status=1.000000,her2_status=0.000000
-# 49.000000 samples in weird combination: er_status=-1.000000,pr_status=-1.000000,her2_status=0.000000
-# 30.000000 samples in weird combination: er_status=1.000000,pr_status=-1.000000,her2_status=0.000000
-# 6.000000 samples in weird combination: er_status=-1.000000,pr_status=1.000000,her2_status=0.000000
-# 51.000000 samples in weird combination: er_status=0.000000,pr_status=0.000000,her2_status=0.000000
-# 0.000000 samples in weird combination: er_status=0.000000,pr_status=0.000000,her2_status=1.000000
-# 0.000000 samples in weird combination: er_status=0.000000,pr_status=0.000000,her2_status=-1.000000
-# ##############################################
-# 78.000000 samples in combination: er_status=1.000000,pr_status=1.000000,her2_status=1.000000  # Luminal B
-# 383.000000 samples in combination: er_status=1.000000,pr_status=1.000000,her2_status=-1.000000 # Luminal A
-# 17.000000 samples in combination: er_status=1.000000,pr_status=-1.000000,her2_status=1.000000
-# 64.000000 samples in combination: er_status=1.000000,pr_status=-1.000000,her2_status=-1.000000
-# 4.000000 samples in combination: er_status=-1.000000,pr_status=1.000000,her2_status=1.000000
-# 11.000000 samples in combination: er_status=-1.000000,pr_status=1.000000,her2_status=-1.000000
-# 19.000000 samples in combination: er_status=-1.000000,pr_status=-1.000000,her2_status=1.000000 # Her2/neu+
-# 117.000000 samples in combination: er_status=-1.000000,pr_status=-1.000000,her2_status=-1.000000 # Triple Negative/Basal cell like
-
-
-
-# try and fix some weird combinations
 
 
 
