f8b5ed1f3959952d6344ad2e47b25fff0af80c83
dan_kufra <dankufra@gmail.com>
2019-12-23 17:39:59 +0200

    Add conv implementation. Random affine still doesn't work for anomaly.

--- receptorStatusPrediction.py
+++ receptorStatusPrediction.py
@@ -3,10 +3,11 @@ from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import torch
-
+import argparse as argparse
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+# import modin.pandas as pd
 
 
 class Net(nn.Module):
@@ -27,6 +28,64 @@ class Net(nn.Module):
                 x = F.relu(layer(x))
             else:
                 x = layer(x)
+        return x
+
+
+class ConvNet(nn.Module):
+    def __init__(self, num_conv_layers=5, num_fully_connected_layers=2, fully_connected_input=128, hidden_dim=128):
+        super(ConvNet, self).__init__()
+        self.layers = nn.ModuleList()
+        for i in range(num_conv_layers):
+            if i == 0:
+                self.layers.append(nn.Conv1d(in_channels=1, out_channels=num_filters,
+                                             kernel_size=kernel_size, padding=(kernel_size // 2)))
+                self.layers.append(nn.MaxPool1d(2))
+            elif i == num_conv_layers - 1:
+                self.layers.append(nn.Conv1d(in_channels=num_filters, out_channels=1,
+                                             kernel_size=kernel_size, padding=(kernel_size // 2)))
+            else:
+                self.layers.append(nn.Conv1d(in_channels=num_filters, out_channels=num_filters,
+                                             kernel_size=kernel_size, padding=(kernel_size // 2)))
+                self.layers.append(nn.MaxPool1d(2))
+        # for i in range(num_fully_connected_layers):
+        #     if i == 0:
+        #         self.layers.append(nn.Linear(transform_dim, hidden_dim))
+        #     elif i == num_fully_connected_layers-1:
+        #         self.layers.append(nn.Linear(hidden_dim, 1))
+        #     else:
+        #         self.layers.append(nn.Linear(hidden_dim, hidden_dim))
+
+    def forward(self, x):
+        import pdb
+        pdb.set_trace()
+        for i, layer in enumerate(self.layers):
+            if i < len(self.layers) - 1:
+                x = F.relu(layer(x))
+            else:
+                x = layer(x)
+        return x
+
+
+class RandomConvNet(nn.Module):
+    def __init__(self, num_downsamples=10, num_filters=1, kernel_size=5):
+        super(RandomConvNet, self).__init__()
+        self.layers = nn.ModuleList()
+        for i in range(num_downsamples):
+            if i == 0:
+                self.layers.append(nn.Conv1d(in_channels=1, out_channels=num_filters,
+                                             kernel_size=kernel_size, padding=(kernel_size // 2)))
+                self.layers.append(nn.MaxPool1d(2))
+            elif i == num_downsamples - 1:
+                self.layers.append(nn.Conv1d(in_channels=num_filters, out_channels=1,
+                                             kernel_size=kernel_size, padding=(kernel_size // 2)))
+            else:
+                self.layers.append(nn.Conv1d(in_channels=num_filters, out_channels=num_filters,
+                                             kernel_size=kernel_size, padding=(kernel_size // 2)))
+                self.layers.append(nn.MaxPool1d(2))
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = layer(x)
         return x
 
 
@@ -53,7 +112,7 @@ class CenterTripletLoss(torch.nn.Module):
         same_center_mask = same_center_mask.scatter_(1, torch.tensor(transform_inds).type(torch.LongTensor), 1)
 
         pull = (torch.sum(dist_mat*same_center_mask, dim=1) + self.margin) * self.pull_lambda
-        push = torch.min(dist_mat[(1 - same_center_mask).type(torch.bool)].reshape(x.shape[0], -1), dim=1)[0] * self.push_lambda
+        push = torch.min(dist_mat[(1 - same_center_mask).type(torch.bool)].reshape(x.shape[0], -1), dim=1)[0] / self.push_lambda
 
         loss = torch.sum(F.relu(pull - push))
         loss /= x.shape[0]
@@ -326,7 +385,6 @@ def classify_triple_negative(df, print_wrong=False):
         print(patients_with_ihc_level_diff)
 
 
-
         patients_wrong_test_svm = df.iloc[shuf_test_idx[np.where(pred_test_her2_svm != Y_test)]][REL_COLS]
         patients_wrong_train_svm = df.iloc[shuf_train_idx[np.where(pred_train_her2_svm != Y_train)]][REL_COLS]
 
@@ -413,15 +471,45 @@ def classify_multiclass(df):
                                                                           multiclass=True, class_names=RECEPTOR_MULTICLASS_NAMES_REDUCED)
 
 
-def transform_samples(X, num_transformations, transform_dim, seed):
-    X_transformed = np.zeros((num_transformations, X.shape[0], transform_dim))
+def transform_samples_array(X_array, num_transformations, transform_dim, seed):
+    X_transformed_array = []
+    for X in X_array:
+        X_transformed_array.append(np.zeros((num_transformations, X.shape[0], transform_dim)))
+
     np.random.seed(seed)
     for transformation in tqdm(np.arange(num_transformations)):
         random_transformation = np.random.randn(transform_dim, X.shape[1])
         random_transformation_bias = np.random.randn(transform_dim, 1)
-        for sample in tqdm(np.arange(X.shape[0])):
-            X_transformed[transformation, sample] = (np.matmul(random_transformation, X[sample].reshape((-1, 1))) + random_transformation_bias).ravel()
-    return X_transformed
+        for i, X, in enumerate(X_array):
+            for sample in tqdm(np.arange(X.shape[0])):
+                X_transformed_array[i][transformation, sample] = (np.matmul(random_transformation, X[sample].reshape((-1, 1))) + random_transformation_bias).ravel()
+    return X_transformed_array
+
+
+def conv_transform_samples_array(X_array, num_transformations, num_downsample=8, num_filters=1, kernel_size=5, seed=555):
+    with torch.no_grad():
+        temp_net = RandomConvNet(int(num_downsample), num_filters=int(num_filters), kernel_size=int(kernel_size))
+        temp_input = torch.from_numpy(np.reshape(X_array[0][0], (1, 1, -1))).float()
+        temp_x_for_size = temp_net.forward(temp_input)
+        X_transformed_array = []
+        for X in X_array:
+            X_transformed_array.append(np.zeros((num_transformations, X.shape[0], temp_x_for_size.shape[2])))
+
+        np.random.seed(seed)
+        # for each random transformation we initialize a random convolutional network with num_downsample layers
+        for transformation in tqdm(np.arange(num_transformations)):
+            random_conv_net = RandomConvNet(int(num_downsample), num_filters=int(num_filters), kernel_size=int(kernel_size))
+            # we run inference with this net on X and save the results
+            for i, X, in enumerate(X_array):
+                size_of_minibatch = 8
+                num_mini_batches = np.ceil(X.shape[0] / float(size_of_minibatch))
+                for sample in tqdm(np.arange(num_mini_batches)):
+                    start_idx = int(sample * size_of_minibatch)
+                    end_idx = int(sample * size_of_minibatch + size_of_minibatch)
+                    current_samples = X[start_idx : end_idx]
+                    current_samples = torch.from_numpy(np.reshape(current_samples, (current_samples.shape[0], 1, -1))).float()
+                    X_transformed_array[i][transformation, start_idx :end_idx] = random_conv_net.forward(current_samples)[:, 0].detach().numpy()
+        return X_transformed_array
 
 
 def run_predict(X, net):
@@ -434,6 +522,17 @@ def run_predict(X, net):
     return net_scores_anomaly
 
 
+def calc_likelihood(scores, centers):
+    likelihood = np.zeros_like(scores)
+    epsilon = 0.000000001
+    for transform_idx in np.arange(scores.shape[0]):
+        for sample_idx in np.arange(scores.shape[1]):
+            numerator = np.exp(-1*np.sqrt((scores[transform_idx, sample_idx] - centers[transform_idx])**2)) + epsilon
+            denominator = np.sum(np.exp(-1*np.sqrt((scores[transform_idx, sample_idx] - centers)**2))) + epsilon*scores.shape[0]
+            likelihood[transform_idx, sample_idx] = numerator / denominator
+    return likelihood
+
+
 def get_anomaly_score(X, net, centers):
     # Predict likelihood of each example
     net_scores = run_predict(X, net)
@@ -444,9 +543,12 @@ def get_anomaly_score(X, net, centers):
 
 
 def train_net(X, num_transformations, hidden_dim, transform_dim, num_layers, batch_size, num_epochs,
-              learning_rate=0.001):
+              lr=0.0001, pull_lambda=1, push_lambda=1, use_conv=False):
     # Learn classifier + centers
-    net = Net(hidden_dim=hidden_dim, transform_dim=transform_dim, num_layers=num_layers).float()
+    if use_conv:
+        net = ConvNet().float()
+    else:
+        net = Net(hidden_dim=hidden_dim, transform_dim=transform_dim, num_layers=num_layers).float()
 
     def init_weights(m):
         if type(m) == nn.Linear:
@@ -454,17 +556,17 @@ def train_net(X, num_transformations, hidden_dim, transform_dim, num_layers, bat
             m.bias.data.fill_(0.01)
 
     net.apply(init_weights)
-
-    criterion = CenterTripletLoss(num_classes=num_transformations, margin=2.0)
-    optimizer = optim.Adam(list(net.parameters()) + list(criterion.parameters()), lr=learning_rate, betas=(0.9, 0.999),
+    criterion = CenterTripletLoss(num_classes=num_transformations, margin=2.0, pull_lambda=pull_lambda, push_lambda=push_lambda)
+    optimizer = optim.Adam(list(net.parameters()) + list(criterion.parameters()), lr=lr, betas=(0.9, 0.999),
                            eps=1e-08, weight_decay=0.9)
-
+    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95, last_epoch=-1)
     epoch_example_num = X.shape[0] * X.shape[1]
     epoch_batches_num = int(epoch_example_num / batch_size)
     print_train = np.round(epoch_batches_num / 4.0)
     print_loss = 0
     print("Starting training, epoch num %d, batches_per_epoch %d" % (num_epochs, epoch_batches_num))
     for epoch in range(num_epochs):
+        scheduler.step(epoch)
         for batch in range(epoch_batches_num):
             if batch % print_train == 0:
                 print("Epoch num %d batch num %d loss %f" % (epoch, batch, print_loss / print_train))
@@ -473,7 +575,11 @@ def train_net(X, num_transformations, hidden_dim, transform_dim, num_layers, bat
             # pick random batch
             transform_inds = np.random.randint(0, X.shape[0], batch_size)
             sample_inds = np.random.randint(0, X.shape[1], batch_size)
-            x = torch.from_numpy(X[transform_inds, sample_inds]).float()
+            if use_conv:
+                x = X[transform_inds, sample_inds]
+                x = torch.from_numpy(np.reshape(x, (x.shape[0], 1, -1))).float()
+            else:
+                x = torch.from_numpy(X[transform_inds, sample_inds]).float()
             # calculate centers
             # centers = calc_centers(net, X_real_train_transformed)
             # run neural network and calculate center triplet loss
@@ -481,25 +587,36 @@ def train_net(X, num_transformations, hidden_dim, transform_dim, num_layers, bat
             loss = criterion(out, centers=None, transform_inds=transform_inds)
             loss.backward()
             optimizer.step()
+            #TODO add accuracy measure?
             print_loss += loss.item()
     print("Finished Training")
     return net, criterion
 
 
-def GOAD(df, num_transformations=5, transform_dim=128, num_epochs=5, batch_size=32, hidden_dim=128, num_layers=5):
+def GOAD(df, use_conv=False, num_transformations=4, transform_dim=5000, num_epochs=20,
+         batch_size=32, hidden_dim=512, num_layers=10, num_sites=350000, seed=None):
+    if seed:
+        np.random.seed(seed)
+    # class_Y = df_to_class_labels(df, classes=CLASSES)
+    # real_df = df[class_Y == 7]
+    # anomaly_df = df[class_Y == 0]
     class_Y = df_to_class_labels(df, classes=CLASSES_REDUCED)
+    real_df = df[class_Y != 2]
+    anomaly_df = df[class_Y == 2]
     # set anomalies class as Triple Negative and real class as others
     print("Starting GOAD")
     # real_df = df[df.pos == 1]
     # anomaly_df = df[df.neg == 1]
-    real_df = df[class_Y != 2]
-    anomaly_df = df[class_Y == 2]
-    num_sites = 5000
-    random_sites = np.sort(np.random.choice(real_df[real_df.columns[['cg' in col for col in real_df.columns]]].values.shape[1], num_sites, replace=False))
+    if num_sites == -1:
+        random_sites = np.arange(real_df[real_df.columns[['cg' in col for col in real_df.columns]]].values.shape[1])
+    else:
+        random_sites = np.sort(np.random.choice(real_df[real_df.columns[['cg' in col for col in real_df.columns]]].values.shape[1], num_sites, replace=False))
     X_real = real_df[real_df.columns[['cg' in col for col in real_df.columns]]].values[:, random_sites].astype(np.float32) / 1000.0
     X_anomaly = anomaly_df[anomaly_df.columns[['cg' in col for col in anomaly_df.columns]]].values[:, random_sites].astype(np.float32) / 1000.0
     # TODO actual random data...should be "easy" to catch as anomaly
-    # X_anomaly = (np.random.randint(0, 1000, X_anomaly.ravel().shape[0]) / 1000.0).reshape((-1, num_sites))
+    # X_anomaly_random = (np.random.randint(0, 1000, X_anomaly.ravel().shape[0]) / 1000.0).reshape((-1, num_sites))
+    X_anomaly_random = X_anomaly[:, np.random.permutation(X_anomaly.shape[1])]
+
     # Create test set that includes part of Triple Negative and part of anomalies
     train_idx = np.zeros(X_real.shape[0], dtype=np.bool)
     train_idx[np.random.choice(np.arange(X_real.shape[0]), int(X_real.shape[0] * 0.8), replace=False)] = True
@@ -508,41 +625,41 @@ def GOAD(df, num_transformations=5, transform_dim=128, num_epochs=5, batch_size=
 
     print("Amount Train: %d, Amount Test: %d, Amount Anomaly: %d" % (len(X_real_train), len(X_real_test), len(X_anomaly)))
     seed = np.random.randint(1000, size=1)
-    print("Starting transformations for real data train")
-    X_real_train_transformed = transform_samples(X_real_train, num_transformations, transform_dim, seed)
+
+    print("Starting transformations for data")
+    if use_conv:
+        transformed_data = conv_transform_samples_array([X_real_train, X_anomaly, X_anomaly_random, X_real_test], num_transformations, 8, seed)
+    else:
+        transformed_data = transform_samples_array([X_real_train, X_anomaly, X_anomaly_random, X_real_test], num_transformations, transform_dim, seed)
+    X_real_train_transformed = transformed_data[0]
+    X_anomaly_transformed = transformed_data[1]
+    X_anomaly_random_transformed = transformed_data[2]
+    X_real_test_transformed = transformed_data[3]
 
     # Learn classifier + centers
-    net, criterion = train_net(X_real_train_transformed, num_transformations, hidden_dim, transform_dim, num_layers, batch_size, num_epochs)
+    net, criterion = train_net(X_real_train_transformed, num_transformations, hidden_dim, X_real_train_transformed.shape[2],
+                               num_layers, batch_size, num_epochs, push_lambda=1, use_conv=use_conv)
     # recalculate centers one last time
     # centers = calc_centers(net, X_real_train_transformed)
     centers = criterion.centers.detach().numpy()
-    print("Starting transformations for anomaly data")
-    # Take anomaly set and apply transformations
-    X_anomaly_transformed = transform_samples(X_anomaly, num_transformations, transform_dim, seed)
+
     score_anomaly = get_anomaly_score(X_anomaly_transformed, net, centers)
+    print("Score anomaly mean: %f" % np.mean(score_anomaly))
 
-    print("Starting transformations for real data test")
-    # Take test set and apply transformations
-    X_real_test_transformed = transform_samples(X_real_test, num_transformations, transform_dim, seed)
+    score_anomaly_random = get_anomaly_score(X_anomaly_random_transformed, net, centers)
+    print("Score anomaly random mean: %f" % np.mean(score_anomaly_random))
+
     score_real_test = get_anomaly_score(X_real_test_transformed, net, centers)
+    print("Score real_test mean: %f" % np.mean(score_real_test))
 
-    # Predict likelihood of each example
     score_real_train = get_anomaly_score(X_real_train_transformed, net, centers)
+    print("Score real_train mean: %f" % np.mean(score_real_train))
 
+    print(centers)
+    plot_centers(centers)
     import pdb
     pdb.set_trace()
     print("Here")
-
-
-def calc_likelihood(scores, centers):
-    likelihood = np.zeros_like(scores)
-    epsilon = 0.00001
-    for transform_idx in np.arange(scores.shape[0]):
-        for sample_idx in np.arange(scores.shape[1]):
-            numerator = np.exp(-1*np.sqrt((scores[transform_idx, sample_idx] - centers[transform_idx])**2)) + epsilon
-            denominator = np.sum(np.exp(-1*np.sqrt((scores[transform_idx, sample_idx] - centers)**2))) + epsilon*scores.shape[0]
-            likelihood[transform_idx, sample_idx] = numerator / denominator
-    return likelihood
 
 
 def calc_centers(net, X):
@@ -555,14 +672,46 @@ def calc_centers(net, X):
     return centers
 
 
+def plot_centers(centers):
+    plt.scatter(centers[:, 0], np.full(centers.shape, 4), marker="x", color='r')
+    plt.show()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tsv_path', type=str, default="",
+                        help='Path to already cleaned tsv file')
+    parser.add_argument('--classify_triple_negative', default=False, action='store_true',
+                        help='Whether to classify triple negative vs non-triple negative')
+    parser.add_argument('--classify_receptor', default=False, action='store_true',
+                        help='Whether to classify each receptor seperately')
+    parser.add_argument('--classify_multiclass', default=False, action='store_true',
+                        help='Whether to classify each BRCA type in multiclass')
+    parser.add_argument('--run_GOAD', default=False, action='store_true',
+                        help='Whether to run GOAD')
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == '__main__':
-    final_df = read_data()
-    get_mismatches(final_df)
-    df_clinical = fix_mismatches(final_df)
-    # classify_triple_negative(df_clinical)
-    # classify_receptor(df_clinical, 'er_ihc')
-    # classify_receptor(df_clinical, 'pr_ihc')
-    # classify_receptor(df_clinical, 'her2_ihc_and_fish')
-    # classify_multiclass(df_clinical)
-    GOAD(df_clinical)
+    args = parse_args()
+    if args.tsv_path == "":
+        final_df = read_data()
+        get_mismatches(final_df)
+        df_clinical = fix_mismatches(final_df)
+    else:
+        df_clinical = pd.read_csv(args.tsv_path, sep='\t', compression='gzip')
+    if args.classify_triple_negative:
+        classify_triple_negative(df_clinical)
+    if args.classify_receptor:
+        classify_receptor(df_clinical, 'er_ihc')
+        classify_receptor(df_clinical, 'pr_ihc')
+        classify_receptor(df_clinical, 'her2_ihc_and_fish')
+    if args.classify_multiclass:
+        classify_multiclass(df_clinical)
+    if args.run_GOAD:
+        GOAD(df_clinical, use_conv=True)
+    import pdb
+    pdb.set_trace()
+    print('here')
 
