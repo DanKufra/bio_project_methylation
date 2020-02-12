0c3864eb7766773501c01e7d290f554ad9627d4b
dan_kufra <dan.kufra@mail.huji.ac.il>
2020-02-12 13:03:56 +0200

    Adding barplot to receptor classification

--- receptorStatusPrediction.py
+++ receptorStatusPrediction.py
@@ -381,9 +381,9 @@ def classify_triple_negative(df, print_wrong=True, run_smote=False):
     2. Observations where IHC level and IHC status did not match
     """
 
-    test_idx = (df['neg_pre_fish'] == df['neg']) & \
-                (df['pos_pre_fish'] == df['pos'])
-    test_idx = test_idx & ((df['her2_ihc'] == df['her2_ihc_level']) &
+    test_idx = (df['neg_pre_fish'] != df['neg']) & \
+                (df['pos_pre_fish'] != df['pos'])
+    test_idx = test_idx & ((df['her2_ihc'] != df['her2_ihc_level']) &
                              (df['her2_ihc_level'] != -2) &
                              ((df['her2_fish'] == -2) | (df['her2_fish'] == 0)))
 
